266239a775d58006d31bfc3993fec026af1cca2f
Jasec <jasec_7@hotmail.com>
2025-02-21 02:29:24 -0800

    complete building simple liker

--- main.js
+++ main.js
@@ -3,6 +3,49 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', ()=>{
+  console.log('the page is loaded')
+  const hide = document.querySelector('#modal');
+  console.log("Modal element:", hide);
+  if (hide) {
+    hide.classList.add("hidden"); 
+    console.log("Modal is now hidden");
+  } else {
+    console.error(" Error: Modal element not found!");
+  };
+
+
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
+  
+  const hearts = document.querySelectorAll('.like-glyph');
+  hearts.forEach(heart => {
+    heart.addEventListener("click", () => {
+      console.log("Heart clicked:", heart); 
+      mimicServerCall()
+        .then(() => {
+        console.log('Server request succesful!');
+        
+        if (heart.textContent === EMPTY_HEART) {
+          heart.textContent = FULL_HEART;
+          heart.classList.add("activated-heart"); 
+        } else {
+          heart.textContent = EMPTY_HEART; 
+          heart.classList.remove("activated-heart"); 
+        }
+      })
+        .catch(() => {
+          console.error("Server request failed");
+          const hide = document.querySelector("#modal");
+          hide.classList.remove("hidden");
+
+          setTimeout(() => {
+            hide.classList.add("hidden");
+          }, 3000);
+    });
+  });
+});
+  
 
 
 
@@ -23,3 +66,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+})
